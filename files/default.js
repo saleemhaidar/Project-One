0b31979c122692f80bec2b2350b46c21be03a57e
Saleem Haidar <saleemhaidar@gmail.com>
2016-06-01 14:57:51 -0700

    Added pictures to the items #1

--- files/default.js
+++ files/default.js
@@ -1,7 +1,7 @@
 var items = [
-  {name: 'Nike Barricade', price:150,},
-  {name: 'Harry Potter', price: 25},
-  {name: 'Hot Wheels Cars', price: 12}
+  {name: 'Nike Barricade', image: 'images/adidasbarricade2016.jpg', price:150,},
+  {name: 'Harry Potter', image: 'images/HarryPotter.jpg', price: 25},
+  {name: 'Hot Wheels Cars', image: 'images/HotWheels.jpeg', price: 12}
 ];
 
 var myButton = document.getElementById('search');
@@ -39,7 +39,13 @@ function match(searchText, list) {
   var name = document.createElement('h3');
   name.textContent = data.name + ' ' + '$' + data.price;
 
+  if (data.image) {
+    var image = document.createElement('img');
+    image.setAttribute('src', data.image);
+    image.setAttribute('class', 'img-responsive col-md-4')
+  }
   container.appendChild(item);
   item.appendChild(name);
+  if (image) item.appendChild(image);
   return container;
 }
