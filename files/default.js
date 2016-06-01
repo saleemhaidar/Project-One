914e463b9611f921a50cfc6a8696e26b78980403
Saleem Haidar <saleemhaidar@gmail.com>
2016-06-01 10:23:40 -0700

    Show matching search results #1

--- files/default.js
+++ files/default.js
@@ -5,19 +5,41 @@ var items = [
 ];
 
 var myButton = document.getElementById('search');
-myButton.addEventListener('click', function(){
+myButton.addEventListener('click', function() {
   var searchText = document.getElementById('searchText');
-  console.log(match(searchText.value, items));
+  show(match(searchText.value, items));
 });
 
 
 // push to issue before moving on to match function.
-function match(searchText, list){
-var suggestions = [];
-list.forEach( function(item){
-  if(item.name.indexOf(searchText) !== -1)
-  suggestions.push(item);
+function match(searchText, list) {
+  var suggestions = [];
+  list.forEach( function(item){
+    if(item.name.indexOf(searchText) !== -1) {
+    suggestions.push(item);
+    }
+  });
+  return suggestions;
+}
 
-})
-return suggestions;
+ function show(items) {
+   var searchResult = document.getElementById('searchResult');
+   for (var i = 0; i < items.length; i++) {
+     searchResult.appendChild(item(items[i]));
+   }
+ }
+
+ function item(data) {
+   var container = document.createElement('div');
+   container.setAttribute('class', 'col-md-2 panel panel-default');
+
+   var item = document.createElement('div');
+   item.setAttribute('class', 'panel-body');
+
+  var name = document.createElement('h3');
+  name.textContent = data.name;
+
+  container.appendChild(item);
+  item.appendChild(name);
+  return container;
 }
